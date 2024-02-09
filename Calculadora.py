151cb8c2876027ad802b1e34adc9446abeb55a9b
Pirmaus <mauriciopirtouscheg@hotmail.com>
2024-02-09 11:13:08 -0300

    Initial commit

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def calculador(a,x,y):
+    if a == 1: return x.sum()
+    if a == 2: return x.sub
+    if a == 3: return x.m
+    if a == 4: return x
+    else: return print ('invalido')
+
+
+a = int(input())
+x = int(input ())
+
+print(calculador(a,x,y))
+
+np.array 
---
+++ importar.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+from io import StringIO    
+  
+a = np.loadtxt("carros-anos.txt") 
+
+print("The loaded array is:")
+  
+print(a) 
